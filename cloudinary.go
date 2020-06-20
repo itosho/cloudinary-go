4530fddd84566b01f34d3399f106da7f60c2c306
itosho <www.itosho@gmail.com>
2020-06-21 03:02:09 +0900

    Add test case

--- cloudinary.go
+++ cloudinary.go
@@ -7,12 +7,13 @@ import (
 )
 
 const (
-	baseUrl             = "https://res.cloudinary.com/"
+	baseUrl             = "https://res.cloudinary.com"
 	defaultResourceType = "image"
 )
 
 type Client struct {
 	cloudName string
+	secure    bool
 	apiKey    string
 	apiSecret string
 }
@@ -20,13 +21,14 @@ type Client struct {
 func NewClient(cloudName string, apiKey string, apiSecret string) Client {
 	return Client{
 		cloudName: cloudName,
+		secure:    true,
 		apiKey:    apiKey,
 		apiSecret: apiSecret,
 	}
 }
 
 func (c *Client) SignUrlByRawTransformation(productId string, accessType string, rawTransformation string) string {
-	seed := rawTransformation + productId + c.apiSecret
+	seed := rawTransformation + "/" + productId + c.apiSecret
 
 	hash := sha1.New()
 	hash.Write([]byte(seed))
@@ -36,9 +38,12 @@ func (c *Client) SignUrlByRawTransformation(productId string, accessType string,
 	signature = strings.Replace(signature, "+", "-", -1)
 	signature = strings.Replace(signature, "/", "_", -1)
 	signature = string([]rune(signature)[:8])
-	signature = "/s--" + signature + "--/"
+	signature = "s--" + signature + "--"
 
 	transformation := strings.Replace(rawTransformation, "\n", "%0A", -1)
 
-	return baseUrl + c.cloudName + defaultResourceType + accessType + signature + transformation + productId
+	signUrl := baseUrl + "/" + c.cloudName + "/" + defaultResourceType + "/" + accessType
+	signUrl = signUrl + "/" + signature + "/" + transformation + "/" + productId
+
+	return signUrl
 }
